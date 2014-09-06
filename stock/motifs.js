813cfe70c6f60d6250d5a1f45df1dfd0de5b5371
ted7726 <ted7726@gmail.com>
2014-09-05 17:37:00 -0700

    motifs.js

---
+++ stock/motifs.js
@@ -0,0 +1,8 @@
+function getMotifs(motif){
+	if (motif === "China Solar"){
+		Update_Table(
+			["CSIQ","TSL","YGE","JKS","DQ","JASO","HSOL","SOL"],
+			[25.25,17.51,13.54,12.99,3.82,14.16,8.07,4.66]
+		);
+	}
+}
