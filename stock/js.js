374da95db533cf2d369cd05c7c889a329b9d27fc
ted7726 <ted7726@gmail.com>
2014-09-08 17:29:16 -0700

    update table

--- stock/js.js
+++ stock/js.js
@@ -1,8 +1,7 @@
+var tableid=1;
+var curr_table = "myTable0";
 
 $(document).ready(function(){
-
-
-
 	$("#submit").click(function(){
 		// analysis input
 		if($("#text1").val() ===""){
@@ -13,7 +12,6 @@ $(document).ready(function(){
 				var matches = input[i].split("	");
 				quotes[i]=matches[0];
 				quotes_weight[i]=parseInt(matches[2]);
-				
 			}
 		}else{
 			var quotes = $("#text1").val().split(",");
@@ -22,75 +20,102 @@ $(document).ready(function(){
 				quotes_weight[i] = 100/quotes.length;
 			}
 		}
-		Update_Table(quotes,quotes_weight);
+		combination(quotes,quotes_weight,"Customized" + tableid);
+		
+		
 	});	
 		
-	
+	$("#submit2").click(function(){
+		$(".datagrid table thead,tbody").fadeToggle();
+	});
 	
 	$("#ChinaSolar").click(function(){
 		getMotifs("ChinaSolar");
+		
 	});
 	
 	$("#ChinaInternet").click(function(){
 		getMotifs("ChinaInternet");
+		
 	});
+	
+	
 	
 });
 
-function Update_Table(quotes,quotes_weight){
-	console.log(quotes);
-	console.log(quotes_weight);
-	if (quotes.length<1) return;
+
+
+function combination(qs,qs_weight,t){
+	
+	this.quotes = qs;
+	this.weight = qs_weight;
+	this.title = t;
+	
+	if (this.quotes.length<1) return;
 	// create table
 	var column = 7;
 	var row = Array(column+1).join("<td></td>")+"</tr>";
 	var rows = "<tbody>";
 	// var title = "<tbody><tr><td>Motif 1</td><tr></tbody>";
 	for(i=0;i<quotes.length;i++){
-		 
 		if(i%2===1) rows+="<tr>"+row;
 		else 		rows+="<tr class = \"alt\">"+row;
 	}
 	rows += "</tbody>";
-	$("#myTable").children("thead").after(rows);
-	// $("#myTable").children("thead").after(title);
-	var url = 'http://finance.google.com/finance/info?client=ig&q='+quotes[0];
+	this.tableID = "myTable"+tableid++;
+	var table = '<div class="title">'+t+'</div><table id="'+this.tableID+'"><thead><tr><th>Symbol</th><th>Price</th><th>Change</th><th>Change %</th><th>Last Trade Time</th><th>change % after</th><th>Last Trade Time after</th></tr></thead>'+rows+'</table>';
+	
+	$("#"+curr_table).after(table);
+	curr_table = this.tableID;
+	this.url = 'http://finance.google.com/finance/info?client=ig&q='+quotes[0];
 	for(i=1;i<quotes.length;i++){
-		url += ","+quotes[i];
+		this.url += ","+quotes[i];
 	}
 	
-	get_quotes_data(url,quotes_weight);
+	get_quotes_data();
 	window.setInterval(function() {
-		get_quotes_data(url,quotes_weight);
-	}, 5000);
+		get_quotes_data();
+	}, 3000);
+	
+	
+	this.get_quotes_data = function(){
+		var tID = this.tableID;
+		var wt = this.weight;
+		var title = this.title;
+		var return_data = $.ajax({
+			url: this.url,
+			dataType: "jsonp",
+			success: function (data){
+				console.log(data);
+				var x=document.getElementById(tID).rows;
+				var weighted_changes_p=0;
+				for(i=0;i<data.length;i++){
+					y=x[i+1].cells;
+					
+					y[0].innerHTML=data[i].t;
+					y[1].innerHTML=data[i].l;
+					y[2].innerHTML=(data[i].c>0? "<red>"+data[i].c+"</red>" : "<green>"+data[i].c+"</green>");
+					y[3].innerHTML=(data[i].cp>0? "<red>"+data[i].cp+"%</red>" : "<green>"+data[i].cp+"%</green>");
+					y[4].innerHTML=data[i].lt;
+					if(typeof(data[i].ecp ) !== "undefined") y[5].innerHTML=(data[i].ecp>0? "<red>"+data[i].ecp+"%</red>" : "<green>"+data[i].ecp+"%</green>");
+					if(typeof(data[i].elt ) !== "undefined")y[6].innerHTML=data[i].elt;
+					// console.log(wt[i]);
+					weighted_changes_p += data[i].cp*wt[i];
+				}
+				// console.log(weighted_changes_p);
+				$("#"+tID).prev().html(
+					(weighted_changes_p >0?"<green>"+Math.round(weighted_changes_p*10)/1000+"%</green>":"<red>"+Math.round(weighted_changes_p)*10/1000+"%</red>")+title
+				);
+				
+				
+			}
+		});
+	};
+	
 }
+
+
 function get_quotes_data(url,quotes_weight){
 	
-	var return_data = $.ajax({
-		url: url,
-		dataType: "jsonp",
-		success: function (data){
-			console.log(data);
-			var x=document.getElementById('myTable').rows;
-			var weighted_changes_p=0;
-			for(i=0;i<data.length;i++){
-				y=x[i+1].cells;
-				
-				y[0].innerHTML=data[i].t;
-				y[1].innerHTML=data[i].l;
-				y[2].innerHTML=(data[i].c>0? "<red>"+data[i].c+"</red>" : "<green>"+data[i].c+"</green>");
-				y[3].innerHTML=(data[i].cp>0? "<red>"+data[i].cp+"%</red>" : "<green>"+data[i].cp+"%</green>");
-				y[4].innerHTML=data[i].lt;
-				if(typeof(data[i].ecp ) !== "undefined") y[5].innerHTML=(data[i].ecp>0? "<red>"+data[i].ecp+"%</red>" : "<green>"+data[i].ecp+"%</green>");
-				if(typeof(data[i].elt ) !== "undefined")y[6].innerHTML=data[i].elt;
-				console.log(quotes_weight[i]);
-				weighted_changes_p += data[i].cp*quotes_weight[i];
-			}
-			console.log(weighted_changes_p);
-			$("#summary").html(weighted_changes_p >0?"<green>"+Math.round(weighted_changes_p*10)/1000+"%</green>":"<red>"+Math.round(weighted_changes_p)*10/1000+"%</red>");
-			$(".datagrid").fadeIn();
-			$("#summary").fadeIn();
-		}
-	});
 	
 }
--- stock/motifs.js
+++ stock/motifs.js
@@ -1,18 +1,21 @@
 function getMotifs(motif){
 	if (motif === "ChinaSolar"){
-		Update_Table(
+		combination(
 			["CSIQ","TSL","YGE","JKS","DQ","JASO","HSOL","SOL"],
-			[25.25,17.51,13.54,12.99,3.82,14.16,8.07,4.66]
+			[25.25,17.51,13.54,12.99,3.82,14.16,8.07,4.66],
+			"China Solar"
 		);
 	}
 	
 	else if (motif === "ChinaInternet"){
-		Update_Table(
+		combination(
 			["BIDU","QIHU","BITA","ATHM","LEJU","WUBA","SINA","SOHU","YOKU","JD","VIPS","CTRP","JMEI","QUNR","TOUR","DANG","NTES","GAME","CYOU","KZ","WB","YY","RENN","SFUN","CMCM"],
-			[20.07,5.38,4.81,4.20,2.74,2.44,1.81,1.11,0.84,13.77,5.93,3.91,3.89,2.63,1.17,1.12,11.11,2.16,1.35,0.64,3.50,1.15,0.85,1.97,1.43]
+			[20.07,5.38,4.81,4.20,2.74,2.44,1.81,1.11,0.84,13.77,5.93,3.91,3.89,2.63,1.17,1.12,11.11,2.16,1.35,0.64,3.50,1.15,0.85,1.97,1.43],
+			"China Internet"
 		);
 	}
 	
+	$(".datagrid").fadeIn();
 }
 
 
