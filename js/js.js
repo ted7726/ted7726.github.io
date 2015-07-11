033fd0ba97ca08d35385f17e9721db73f14ba322
WeiSheng Su <wilsonsu@yahoo-inc.com>
2015-07-10 23:59:43 -0700

    update grid overlap

--- js/js.js
+++ js/js.js
@@ -176,7 +176,7 @@ $(document).ready(function(){
 		// $(".newsContainer").animate({display:'block'});
 	});
 	
-	$(".list")	.click(function(){	addMotif(this.id);	});
+	$(".list").click(function(){	addMotif(this.id);	});
 	$("#testSearch").autocomplete({
 		source: ["Shale Oil","Frack Attack","Finding Momo","Dr Copper","Black Gold","Big Data","Bear International Market","Bear US Market","Bear US Sectors","Shale Gas","No Glass Ceilings","Biotech Breakthroughs","Social Networking","High Spirits","Natural Gas Glut","Onward Online Ads","Drug Patent Cliffs","Office Space","Online Gaming World","Energetic MLPs","Battling Cancer","Used Car Tune up","Recycled Steel","Hot Retail","Low Beta","Tablet Takeover","Precious Metals","Repeal Obamacare","Dividend Stars","Caffeine Fix","Junk Foods","Income Inequality","Defensive Dividends","Home Improvement","Dogs of the Dow","Discount Nation","China Internet","Chinese Solar","Cleantech Everywhere","Couch Commerce","Rest In Peace","Retiring 2055","High Yield Dividends","Sell in May","Retiring 2050","New Era Portfolio","7Twelve Core Portfolio","Utility Bills","Bullet Proof Balance Sheets","Retiring 2045","All American","Classic 60 40","Retiring 2040","Nuclear Renaissance","Cash Flow Kings","Lots of Likes","Fossil Free","On The Road","Modern Warfare","Growing Dividends","Retiring 2035","Renter Nation","Digital Dollars","Ivy League","BRICS Building","Retiring 2030","Socially Responsible","Pet Passion","Online Video","No Brainer Portfolio","Higher Highs","Healthy and Tasty","Retiring 2025","Lazy 3 Portfolio","Electronic Trading","Vanity Flair","Senior Care","Disappointing the Street","Content is King","Horizon Model 5 Year Aggressive","Horizon Model 15 Year Aggressive","Horizon Model 1 Year Aggressive","Horizon Model 5 Year Moderate","Horizon Model 1 Year Moderate","Horizon Model 15 Year Moderate","Index Fans","GARP","Childs Play","Robotic Revolution","Transporting America","Small Cap Stars","Democratic Donors","World of Sports","No Brainer Portfolio","No Glass Ceilings","Nuclear Renaissance","Obamacare","Office Space","Online Gaming World","Online Video","On The Road","Onward Online Ads","Permanent Strategy","Pet Passion","Precious Metals","Private Equity","Property Casualty Insurance","QE Japan","Recent IPOs","Recycled Steel","Renter Nation","Repeal Obamacare","Republican Donors","Rest In Peace","Retiring 2020","Retiring 2025","Retiring 2030","Retiring 2035","Retiring 2040","Retiring 2045","Retiring 2050","Retiring 2055","Rising Food Prices1","Rising Interest Rates","Robotic Revolution","Senior Care","Seven Deadly Sins","Shale Gas","Shale Oil","Small Cap Stars","Smart Grid","Socially Responsible","Social Networking","Software as a Service","Spinoffs","Stable Earnings","Tablet Takeover","Taking Flight","Tax Inversion Targets","Tech Takeout Targets","That New Car Smell","Too Big to Fail","Transporting America","Used Car Tune up","US Treasury Ladder","Utility Bills","Vanity Flair","Wall Street","Water Shortage","Wearable Tech","World of Sports","CustomDrugPatentCliffs"]
     });
@@ -184,7 +184,7 @@ $(document).ready(function(){
 	getMarketNews();
 	var favorites = getFavorites();
 	for(var i=0;i<favorites.length;i++){
-		new quote(favorites[i],i==favorites.length-1);
+		new quote(favorites[i],i==(favorites.length-1));
 	}
 	
 	/* Facebook and Google+ like button: */
@@ -568,9 +568,13 @@ function quote(tick,isUpdatingQuotes){
 			dataType: "jsonp",
 			success: function (data){
 				var quote = data.query.results.quote;
-				if(quote == null){
+
+				if(quote == null || quote.Name == null){
+					this.quoteContainer.remove();
+					this.quoteContainerFake.remove();
 					return;
 				}
+
 				if(parseFloat(quote.LastTradePriceOnly)>10){
 					quote.Change = Math.round(parseFloat(quote.Change)*100)/100;
 					quote.LastTradePriceOnly = Math.round(parseFloat(quote.LastTradePriceOnly)*100)/100;
@@ -607,6 +611,7 @@ function updateQuoteContainerOffset(quoteContainer,quoteContainerFake){
 			left:quoteContainerFake.offset().left-10}
 	);
 	quoteContainerFake.height(quoteContainer.height());
+
 }
 
 function loadQuoteFaileHandler(quoteContainer){
@@ -731,6 +736,7 @@ function update_INDEXs(){
 			}
 		}
 	});
+	screenSizeChanged();
 }
 
 function update_detail(){
@@ -994,9 +1000,12 @@ function loadBasicChart(tick,c,isUpdatingQuotesGrid){
 
 
 			//quoteContainerFake.height(quoteContainer.height());
-			if(isUpdatingQuotesGrid){
-				screenSizeChanged();
-			}
+
+			updateQuoteContainerOffset(c.parent(),c.parent().next());
+
+			//if(isUpdatingQuotesGrid){
+			//	screenSizeChanged();
+			//}
 		}
 	});
 }
